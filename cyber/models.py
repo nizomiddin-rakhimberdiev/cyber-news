a6827e0406d1406d5315603a7392d018daf2121c
nizomiddin-rakhimberdiev <nizomiddinoff@mail.ru>
2024-05-07 09:38:40 +0500

    add some models

--- cyber/models.py
+++ cyber/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-# Create your models here.
 class Company(models.Model):
     name = models.CharField(max_length=100)
     country = models.CharField(max_length=100)
@@ -13,20 +12,81 @@ class Game(models.Model):
     name = models.CharField(max_length=100)
     founder_company = models.ForeignKey(Company, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
-
     def __str__(self):
         return self.name
 
-
 class Gamer(models.Model):
     full_name = models.CharField(max_length=100)
-    nickname = models.CharField(max_length=100, unique=True)
+    nickname = models.CharField(max_length=100 , unique=True)
     rating = models.IntegerField(default=0)
     avatar = models.ImageField(upload_to='static/assets/avatars/')
-    game = models.ForeignKey(Game, on_delete=models.CASCADE)
+    game = models.ForeignKey(Game , on_delete=models.CASCADE)
+    def __str__(self):
+        return self.full_name
 
-    def  __str__(self):
-        return self.nickname
+class Group(models.Model):
+    name = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    image_logo = models.ImageField(upload_to='static/assets/images/')
+    def __str__(self):
+        return self.name
 
+class Gamer_Group(models.Model):
+    name = models.CharField(max_length=100)
+    group_id = models.ForeignKey(Group, on_delete=models.CASCADE)
+    gamer_id = models.ForeignKey(Gamer, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.name
 
+class Game_Club(models.Model):
+    name = models.CharField(max_length=100)
+    address  = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    image = models.ImageField(upload_to='static/assets/images/')
+    def __str__(self):
+        return self.name
 
+class Battle(models.Model):
+    title = models.CharField(max_length=100)
+    battle_data = models.DateTimeField(auto_now_add=True)
+    game_club_id = models.ForeignKey(Game_Club, on_delete=models.CASCADE)
+    battle_time = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+class Battle_Group(models.Model):
+    group_id=models.ForeignKey(Group, on_delete=models.CASCADE)
+    battle_id=models.ForeignKey(Battle, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.group_id.name} {self.battle_id.title}"
+
+class Battle_News(models.Model):
+    battle_id = models.ForeignKey(Battle, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+class Battle_News_Images(models.Model):
+    image = models.ImageField(upload_to='static/assets/images/')
+    battle_id = models.ForeignKey(Battle, on_delete=models.CASCADE)
+    battle_news_id = models.ForeignKey(Battle_News, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.battle_news_id.title
+
+class Updates_news(models.Model):
+    game_id = models.ForeignKey(Game, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    video = models.FileField(upload_to='static/assets/videos/')
+    image = models.ImageField(upload_to='static/assets/images/')
+
+    def __str__(self):
+        return self.title
