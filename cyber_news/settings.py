b618e807d5104b9d8881e9f581b5bb2c6cb9be15
nizomiddin-rakhimberdiev <nizomiddinoff@mail.ru>
2024-06-04 09:25:59 +0500

    add some finish changes

--- cyber_news/settings.py
+++ cyber_news/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-s)drz90-)dd@%2cqdpyc+t8y^01)a@8-ay&btbrxi-e6vn6z5l"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
